4e08e04a8d6a3e4d53efe0038e2723b1c89c6ae0
Alessio Dal Santo <alessiodalsanto@live.it>
2017-08-24 16:52:30 +0200

    Inizio dichiarazione

--- MeteoStation/MeteoStation.ino
+++ MeteoStation/MeteoStation.ino
@@ -4,6 +4,28 @@
  Author:	aless
 */
 
+#include <LiquidCrystal_I2C.h>
+
+#pragma region dichiarazioni
+
+#define TempInt_Source 1 
+#define D_Units "kmh"
+#define Mode 0
+
+boolean Solar_UV = false;
+float TemperaturaInt_offset = 0.0;			//Interior Temperature Fine Adjust
+float pressure_offset = -4.0;				//Pressure Fine Adjust
+float Local_Altitude = 70.0;				//Insert here altitude of your location in meters
+
+unsigned long interval = 23000;				//Don't change
+
+LiquidCrystal_I2C dlc(0x27, 20, 4);
+byte lcd_L4_info = 1;
+
+#pragma endregion
+
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
 
@@ -11,5 +33,5 @@ void setup() {
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+
 }
