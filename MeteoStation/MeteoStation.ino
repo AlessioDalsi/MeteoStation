799a158da5747e0942c1f8c716c7266f09c279f9
Alessio Dal Santo <Alessio Dal Santo>
2017-08-24 16:16:09 +0200

    Aggiungere i file di progetto.

---
+++ MeteoStation/MeteoStation.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MeteoStation.ino
+ Created:	24/08/2017 16:15:56
+ Author:	aless
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
