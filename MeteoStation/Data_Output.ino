bd45153ec33b8241a345b8ffd6ba3b071fb15625
Alessio Dal Santo <alessiodalsanto@live.it>
2017-09-05 18:11:37 +0200

    Inizio modifiche alla ricezione e la decodifica dei dati

--- MeteoStation/Data_Output.ino
+++ MeteoStation/Data_Output.ino
@@ -1,8 +1,5 @@
 void Data_Output() {
 	digitalWrite(Led2_PIN, HIGH);
-	if (TempInt_Source == 0) {
-		TemperaturaInt = TempIntBMP;
-	}
 	digitalWrite(Led2_PIN, LOW);
 
 	write_LCD();
--- MeteoStation/RCode.ino
+++ MeteoStation/RCode.ino
@@ -1,22 +1,30 @@
 String RCode() {
+
+
 	String code = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 	long startMicros = micros(), endMicros;
+
 	if (bitRead(PINB, 0)) return "";
+
 	while (!bitRead(PINB, 0))
 	{
 		if ((micros() - startMicros) > smax)
 			return "";
 	}
+
 	if ((micros() - startMicros) < smin)
 		return "";
+
 	startMicros = micros();
 	while (bitRead(PINB, 0))
 	{
 		if ((micros() - startMicros) > semax)
 			return "";
 	}
+
 	if ((micros() - startMicros) < semin)
 		return "";
+
 	for (int i = 0; i < nobits; i++)
 	{
 		startMicros = micros();
@@ -25,17 +33,21 @@ String RCode() {
 			if ((micros() - startMicros) < 1);
 		}
 		endMicros = micros();
+
 		if (((endMicros - startMicros) > lmin) && ((endMicros - startMicros) < lmax))
 			code.setCharAt(i, '0');
+
 		else if (((endMicros - startMicros) > hmin) && ((endMicros - startMicros) < hmax))
 			code.setCharAt(i, '1');
 
 		startMicros = micros();
+
 		while (bitRead(PINB, 0))
 		{
 			if ((micros() - startMicros) > semax)
 				return "";
 		}
+
 		if ((micros() - startMicros) < semin)
 			return "";
 	}
